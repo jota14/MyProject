29049bc917b52b5edda25c11bbc7e28a9e86f2b8
jota14 <jozetqp@outlook.es>
2017-11-09 21:24:13 +0100

    Proyecto commit

---
+++ MyProjectJavaFx/src/application/Controlador.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controlador {
+	AnchorPane anchor;
+    @FXML
+    private Button btnDie;
+
+    @FXML
+    private Button btnLive;
+
+    @FXML
+    void abrirImg(ActionEvent event) {
+    
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("VistaLive.fxml"));
+			anchor = loader.load();
+			Stage sendStage = new Stage();
+            sendStage.setTitle("Superheader");
+            
+            Scene scene = new Scene(anchor);
+            sendStage.setScene(scene);
+            sendStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void cerrarVentana(ActionEvent event) {
+    	Platform.exit();
+    }
+}
---
+++ MyProjectJavaFx/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	AnchorPane anchor;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("Vista.fxml"));
+			anchor = loader.load();
+			Scene scene = new Scene(anchor,720,450);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("My App");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
